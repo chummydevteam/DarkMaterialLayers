8eda509b9131b0b7fe4aec811cc105a1b6372679
adityaakadynamo <adityaakadynamo@gmail.com>
2015-11-06 22:18:36 +0530

    Fresh Base
    Started the Project with a new Base

--- Dark Material - Calculator/theme/gen/com/android/calculator2/darkmaterial/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.android.calculator2.darkmaterial;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
