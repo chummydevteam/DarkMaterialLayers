ab45c02457e968b55c575341b693af0d69871ef2
adityaakadynamo <adityaakadynamo@gmail.com>
2015-11-19 16:28:46 +0530

    Updated Kernel Adiutor

--- Layers Development/Kernel Adiutor/hangouts-overlay/gen/com/grarak/kerneladiutor/darkmaterial/R.java
+++ Layers Development/Kernel Adiutor/hangouts-overlay/gen/com/grarak/kerneladiutor/darkmaterial/R.java
@@ -1,6 +1,6 @@
 /*___Generated_by_IDEA___*/
 
-package com.grarak.kernelauditor.darkmaterial;
+package com.grarak.kerneladiutor.darkmaterial;
 
 /* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
 public final class R {
